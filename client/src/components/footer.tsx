4172e3f3dbcc411c22ee5ce7556c6912682326a1
usagisean <164856941+usagisean@users.noreply.github.com>
2026-04-26 10:43:26 +0800

    Update footer text to 'ZXAIHub'

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -102,7 +102,7 @@ function Footer() {
                             setDoubleClickTimes(doubleClickTimes + 1)
                         }
                     }}>
-                        © {new Date().getFullYear()} Powered by <a className='hover:underline' href="https://github.com/openRin/Rin" target="_blank">Rin</a>
+                        © {new Date().getFullYear()} ZXAIHub
                     </span>
                     {config.getBoolean('rss') && <>
                         <Spliter />
